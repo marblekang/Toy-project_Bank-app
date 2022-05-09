7516432b0f319e955d3739acfad3fc8916be18b5
marblekang <kangbm930@gmail.com>
2022-05-10 02:29:29 +0900

    feat: JSON 데이터 전체 다 바인딩

--- js/app.js
+++ js/app.js
@@ -1,21 +1,77 @@
-// dataList 변수로 json 데이터를 받아서 ,
-//그 안에 bankList 라는 객체 배열에 접근하고 싶습니다 ㅠㅠ
-
-//json 데이터 가져오기
-/* async function fetchList() {
+async function fetchList() {
   let response = await fetch(
-    `https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f6e4d3d3-c52c-4ea8-b665-968a3b17c5ea/bank.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220506%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220506T071024Z&X-Amz-Expires=86400&X-Amz-Signature=b64692ad33e835d4c2d70f4c02e816a19d8f2d396044b54537386174238ceaf5&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22bank.json%22&x-id=GetObject`
+    `https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f6e4d3d3-c52c-4ea8-b665-968a3b17c5ea/bank.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220508%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220508T122053Z&X-Amz-Expires=86400&X-Amz-Signature=bd8a004f23f2ef917225bce8bd5465e7a47fcac6769dfdbfb44ca0b2a48bcb61&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22bank.json%22&x-id=GetObject`
   );
   const obj = await response.json();
-  console.log(obj.bankList[0]);
-  //여기서 뭔가 실행
-  for (let i = 0; i < obj.bankList.length; i++) {
-    const ulList = document.createElement("ul");
-    ulList.setAttribute("class", "ul-list");
+  console.log(obj.bankList);
 
-    const liList = document.createElement("li");
-    liList.setAttribute("class", "li-list");
+  //날짜 중복 없이 추출
+  let newDate;
+  let sum = 0;
+  function drawDate() {
+    let temp = [];
+    for (let i = 0; i < obj.bankList.length; i++) {
+      temp.push(obj.bankList[i].date);
+    }
+    newDate = [...new Set(temp)];
+  }
+  drawDate();
+  ////////////////////
+  console.log(newDate);
 
+  const dateMap = obj.bankList.reduce((previousValue, currentValue) => {
+    const currDate = currentValue.date;
+    previousValue[currDate] = Object.keys(previousValue).includes(currDate)
+      ? [...previousValue[currDate], currentValue]
+      : [currentValue];
+    return previousValue;
+  }, {});
+
+  const newSum = Object.keys(dateMap).reduce(
+    (previousValue, currentValue) => [...previousValue, dateMap[currentValue]],
+    []
+  );
+  ////////////////////////
+
+  // /e.date 가 일치하는 데이터의 price만 출력해서 sum
+  let priceSumArr = [];
+  for (let i = 0; i < newDate.length; i++) {
+    priceSumArr[i] = 0;
+    for (let j = 0; j < newSum[i].length; j++) {
+      if (newSum[i][j].income == "out") {
+        priceSumArr[i] -= newSum[i][j].price;
+      } else {
+        priceSumArr[i] += newSum[i][j].price;
+      }
+    }
+  }
+
+  console.log(priceSumArr);
+
+  ////////////////////////////////////////
+
+  // 날짜별로 item만 뽑아서 배열로
+  let items = []; //30
+  for (let i = 0; i < newDate.length; i++) {
+    items[i] = [];
+    for (let j = 0; j < newSum[i].length; j++) {
+      items[i].push(newSum[i][j].history);
+    }
+  }
+  ///////////////////////////////////
+
+  // 날짜별 지출액만 뽑아서 배열로
+  let priceList = []; //30
+  for (let i = 0; i < newDate.length; i++) {
+    priceList[i] = [];
+    for (let j = 0; j < newSum[i].length; j++) {
+      priceList[i].push(newSum[i][j].price);
+    }
+  }
+  /////////////////////////////////
+
+  const todayHistory = document.querySelector(".today-history");
+  for (let i = 0; i < newDate.length; i++) {
     const historyTitle = document.createElement("div");
     historyTitle.setAttribute("class", "history-title");
 
@@ -25,103 +81,59 @@
     const sumSpending = document.createElement("span");
     sumSpending.setAttribute("class", "sum-spending");
 
-    const dailyList = document.createElement("div");
-    dailyList.setAttribute("class", "daily-list");
+    const divEl = document.createElement("div");
 
-    const item = document.createElement("span");
-    item.setAttribute("class", "daily-list-item");
+    todayHistory.appendChild(divEl);
 
-    const price = document.createElement("span");
-    price.setAttribute("class", "daily-list-price");
-
-    const todayHistory = document.querySelector(".today-history");
-
-    // append
-    todayHistory.appendChild(ulList);
-
-    ulList.appendChild(liList);
-
-    liList.appendChild(historyTitle);
+    divEl.appendChild(historyTitle);
 
     historyTitle.appendChild(date); //오늘 2일전 3일전
-
+    date.innerHTML = newDate[i];
     historyTitle.appendChild(sumSpending); //사용금액 합계
+    sumSpending.innerHTML = `Sum: ${priceSumArr[i]}`;
 
-    liList.appendChild(dailyList);
+    for (let j = 0; j < newSum[i].length; j++) {
+      const ulList = document.createElement("ul");
+      ulList.setAttribute("class", "ul-list");
 
-    dailyList.appendChild(item); //건 별 사용내역
-    item.textContent = obj.bankList[i].history;
-    dailyList.appendChild(price); // 건 별 사용금액
-    price.textContent = obj.bankList[i].price;
+      const liList = document.createElement("li");
+      liList.setAttribute("class", "li-list");
+
+      const dailyList = document.createElement("div");
+      dailyList.setAttribute("class", "daily-list");
+
+      const item = document.createElement("span");
+      item.setAttribute("class", "daily-list-item");
+
+      const price = document.createElement("span");
+      price.setAttribute("class", "daily-list-price");
+
+      divEl.appendChild(ulList);
+
+      ulList.appendChild(liList);
+
+      liList.appendChild(dailyList);
+
+      dailyList.appendChild(item); //건 별 사용내역
+      item.innerText = items[i][j];
+      dailyList.appendChild(price); // 건 별 사용금액
+      if (newSum[i][j].income === "out") {
+        price.innerText = `-${priceList[i][j]}`;
+      } else if (newSum[i][j].income === "in") {
+        price.innerText = `+${priceList[i][j]}`;
+      }
+    }
   }
+
+  console.log(newSum);
 }
 
 fetchList();
 
-/*
-function setClassName(el, name) {
-  el.setAttribute("class", name);
-}
- */
-
-const dragIcon = document.querySelector(".slider-container img");
-dragIcon.addEventListener("click", function () {
-  console.log("click!");
+const dragBtn = document.querySelector(".slider-container");
+dragBtn.addEventListener("click", function () {
+  document.querySelector(".history").classList.toggle("drag-change");
+  document
+    .querySelector(".daily-history-container")
+    .classList.toggle("height-change");
 });
-
-async function fetchList() {
-  let response = await fetch(
-    `https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f6e4d3d3-c52c-4ea8-b665-968a3b17c5ea/bank.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220508%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220508T122053Z&X-Amz-Expires=86400&X-Amz-Signature=bd8a004f23f2ef917225bce8bd5465e7a47fcac6769dfdbfb44ca0b2a48bcb61&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22bank.json%22&x-id=GetObject`
-  );
-  const obj = await response.json();
-  console.log(obj.bankList[0]);
-
-  const ulList = document.createElement("ul");
-  ulList.setAttribute("class", "ul-list");
-
-  const historyTitle = document.createElement("div");
-  historyTitle.setAttribute("class", "history-title");
-
-  for (let i = 0; i < obj.bankList.length; i++) {
-    const liList = document.createElement("li");
-    liList.setAttribute("class", "li-list");
-
-    const date = document.createElement("span");
-    date.setAttribute("class", "date");
-
-    const sumSpending = document.createElement("span");
-    sumSpending.setAttribute("class", "sum-spending");
-
-    const dailyList = document.createElement("div");
-    dailyList.setAttribute("class", "daily-list");
-
-    let item = document.createElement("span");
-    item.setAttribute("class", "daily-list-item");
-
-    let price = document.createElement("span");
-    price.setAttribute("class", "daily-list-price");
-
-    const todayHistory = document.querySelector(".today-history");
-
-    // append
-    todayHistory.appendChild(ulList);
-
-    ulList.appendChild(liList);
-
-    liList.appendChild(historyTitle);
-
-    historyTitle.appendChild(date); //오늘 2일전 3일전
-
-    historyTitle.appendChild(sumSpending); //사용금액 합계
-
-    liList.appendChild(dailyList);
-
-    dailyList.appendChild(item); //건 별 사용내역
-    item.textContent = obj.bankList[i].history;
-    dailyList.appendChild(price); // 건 별 사용금액
-    price.textContent = obj.bankList[i].price;
-    console.log(item);
-  }
-}
-
-fetchList();
